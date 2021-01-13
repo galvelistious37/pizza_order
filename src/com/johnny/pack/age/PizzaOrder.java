235f3adeff4405ff9397684f17c76406f6d53aa5
galvelistious37 <galvelistious37@gmail.com>
2021-01-12 21:40:38 -0800

    Initial commit

---
+++ src/com/johnny/pack/age/PizzaOrder.java
@@ -0,0 +1,16 @@
+package com.johnny.pack.age;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PizzaOrder extends Application {
+
+    public static void main(String[] args){
+
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+}
